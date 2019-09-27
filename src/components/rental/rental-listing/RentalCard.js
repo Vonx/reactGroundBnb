719f8b786523f4b618194cd0ce0ca89a5be8ceb7
ryan <bapezryan84@bellevuecollege.edu>
2019-09-26 19:07:46 -0700

    hover message added on image hover, color fixes

--- src/components/rental/rental-listing/RentalCard.js
+++ src/components/rental/rental-listing/RentalCard.js
@@ -9,7 +9,10 @@ export function RentalCard(props) {
         <div className={props.colNum}>
             <Link className='rental-detail-link' to={`/rentals/${rental._id}`}>
                 <div className='card bwm-card'>
+                    <div className='img-block'>
+                        <h4 className='card-hover-text'>Click to view more</h4>
                     <img className='card-img-top' src={rental.image} alt={rental.title}></img>
+                    </div>
                     <div className='card-block'>
                         <h6 className={`card-subtitle ${rental.category}`}>{rentalType(rental.shared)} {rental.category} &#183; {rental.city}</h6>
                         <h4 className='card-title'>{rental.title}</h4>
